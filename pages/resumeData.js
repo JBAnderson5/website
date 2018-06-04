58a1d40f355ef92b2e5b45668578cf6756a4e6b4
JBAnderson5 <josephbecheranderson@gmail.com>
2018-06-03 22:05:43 -0700

    exported resume data onto it's own js file

---
+++ pages/resumeData.js
@@ -0,0 +1,1 @@
+var homepageBlurb="I'm a Californian based <span>Software Engineer</span>,<span>illustrator</span> and <span>webdesigner</span> creating awesome andeffective visual identities for companies of all sizes around the globe.<a class="smoothscroll" href="#about">Start scrolling</a>and learn more <a class="smoothscroll" href="#about">about me</a>.";
